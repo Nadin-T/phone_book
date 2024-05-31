eac5ee25306bd9fecd4baa6b9baec4f8e8077c0c
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-06-01 02:02:22 +0300

    add func standart_write, remove_row

--- phone_book.py
+++ phone_book.py
@@ -17,6 +17,7 @@ class NameError(Exception):
     def __init__(self, txt):
         self.txt = txt
 
+
 def get_info():
     flag = False
     while not flag:
@@ -50,17 +51,32 @@ def write_file(file_name):
     new_obj = {'first_name': user_data[0], 'second_name': user_data[1], 
                'phone_number': user_data[2]}
     res.append(new_obj)
-    with open(file_name, 'w', encoding='utf-8') as data:
-        f_w = DictWriter(data, fieldnames=['first_name', 'second_name',
-                                           'phone_number'])
-        f_w.writeheader()
-        f_w.writerows(res)
+    standart_write(file_name, res)
 
 
 def read_file(file_name):
     with open(file_name, encoding='utf-8', newline='') as data:
         f_r = DictReader(data)
         return list(f_r) # список со словарями
+    
+
+def remove_row(file_name):
+    search = int(input('Введите номер строки для удаления: '))
+    res = read_file(file_name)
+    if search <= len(res):
+        res.pop(search-1)
+        standart_write(file_name, res)
+    else:
+        print('Введен неверный номер строки!')
+
+
+def standart_write(file_name, res):
+    with open(file_name, 'w', encoding='utf-8') as data:
+    f_w = DictWriter(data, fieldnames=['first_name', 'second_name',
+                                            'phone_number'])
+    f_w.writeheader()
+    f_w.writerows(res)
+
 
 file_name = 'phone.csv'
 def main():
@@ -77,6 +93,11 @@ def main():
                 print('Файл отсутствует, создайте его.')
                 continue
             print(*read_file(file_name))
+        elif command == 'd':
+            if not exists(file_name):
+                print('Файл отсутствует, создайте его.')
+                continue
+            remove_row(file_name)
             
 
 main()
